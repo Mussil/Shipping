de33949d524b11a6794a7825d7048c0a6d9d783f
Your Name <ruticohen770@gmail.com>
2022-02-25 12:39:38 +0200

    temp

--- flasker/app.py
+++ flasker/app.py
@@ -1,28 +1,93 @@
 from flask import Flask
 from flask import render_template
 from flasker.SPutils import sp
+import datetime
+import json
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def map():
-#     # print(sp.listOfFidCoords())
-#     return render_template('map.html')
-
-
 @app.route('/')
-def showMap():
-    path1 = [[34.66298873986377, 31.77537251843408], [34.66385643448008, 31.775544673159242], [34.66385643448008, 31.775544673159242], [34.66408404847699, 31.778544880450557], [34.65998456814748, 31.80985125376073], [34.65991666060499, 31.810352776309365], [34.658266577330146, 31.8095198785306], [34.65680726524424, 31.811224410665826], [34.654529037201335, 31.812626772099204], [34.65551509672302, 31.81477667621152]] #  TODO: send this temp path to client and represent it
-    pathsFid=[1, 2, 12, 6, 7, 8, 17, 18, 19]
-    pathsFid=[15, 14, 6, 7, 17, 8, 18, 19]
-    # pathsFid=[21, 20, 14, 15, 68, 29]
-    path1=list(map(lambda x: sp.getStationCoords(x), pathsFid ))
-    return render_template('map.html',stations=list(sp.getListOfCoords()), path=path1)
+def showMapForAll():
 
+    paths = [
+                {
+                    "driver": 1,
+                    "start": datetime.datetime(2022, 1, 2, 3, 29, 0),
+                    # "start":  3,
 
+                    "path": [
+                        21,
+                        20,
+                        8,
+                        17,
+                        18,
+                        6,
+                        41
+                    ],
+                    "times": [
+                        datetime.datetime(2022, 1, 2, 3, 29, 0),
+                        datetime.datetime(2022, 1, 2, 3, 29, 31),
+                        datetime.datetime(2022, 1, 2, 3, 32, 29),
+                        datetime.datetime(2022, 1, 2, 3, 32, 59),
+                        datetime.datetime(2022, 1, 2, 3, 33, 38),
+                        datetime.datetime(2022, 1, 2, 3, 35, 2),
+                        datetime.datetime(2022, 1, 2, 3, 37, 50),
+                        # "2022-01-02 03:29:00",
+                        # "2022-01-02 03:29:31",
+                        # "2022-01-02 03:32:29",
+                        # "2022-01-02 03:32:59",
+                        # "2022-01-02 03:33:38",
+                        # "2022-01-02 03:35:02",
+                        # "2022-01-02 03:37:50"
+                    ]
+                },
+                {
+                    "driver": 2,
+                    "start": datetime.datetime(2022, 1, 2, 9, 38, 0),
+                    # "start":  1,
+                    "path": [
+                        62,
+                        14,
+                        15,
+                        13
+                    ],
+                    "times": [
+                        datetime.datetime(2022, 1, 2, 9, 38, 0),
+                        datetime.datetime(2022, 1, 2, 9, 45, 59),
+                        datetime.datetime(2022, 1, 2, 9, 46, 2),
+                        datetime.datetime(2022, 1, 2, 9, 54, 57),
+                        # "2022-01-02 09:38:00",
+                        # "2022-01-02 09:45:59",
+                        # "2022-01-02 09:46:02",
+                        # "2022-01-02 09:54:57"
+                    ]
+                },
+                {
+                    "driver": 3,
+                    "start": datetime.datetime(2022, 1, 2, 23, 52, 0),
+                    # "start":  4,
+                    "path": [
+                        6,
+                        4,
+                        14,
+                        15,
+                        69
+                    ],
+                    "times": [
+                        datetime.datetime(2022, 1, 2, 23, 52, 0),
+                        datetime.datetime(2022, 1, 2, 23, 54, 46),
+                        datetime.datetime(2022, 1, 2, 23, 59, 33),
+                        datetime.datetime(2022, 1, 2, 23, 59, 36),
+                        datetime.datetime(2022, 1, 2, 0, 6, 23),
+                        # "2022-01-02 23:52:00",
+                        # "2022-01-02 23:54:46",
+                        # "2022-01-02 23:59:33",
+                        # "2022-01-02 23:59:36",
+                        # "2022-01-03 00:06:23"
+                    ]
+                }
+            ]
 
-@app.route('/try')
-def map2():
-    return render_template('map2.html')
+    return render_template('map.html', stations=sp.stations, paths=paths)
 
 
--- flasker/pathCalc.py
+++ flasker/pathCalc.py
@@ -1,12 +1,7 @@
-from pprint import pprint
-
 import WazeRouteCalculator
 import json
 import time
 import datetime
-from helpers import access_token
-
-from mapbox import Directions
 
 from flasker.helpers import initialDate
 
@@ -86,18 +81,33 @@ def stationTimes(station_name):
         for station2 in range(1, 71): # end stations
 
             if station_name != station2:
-                stationsJson[f'{station_name}-{station2}'] = getDistTime(station_name, station2, datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 12, 0))
+                time0, dist0 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 1,30))  # 23:00 - 3:00
+                time1, dist1 = getDistTime(station_name, station2, datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 4,30))  # 3:00 - 6:00
+                time2, dist2 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 7,0))  # 6:00 - 8:00
+                time3, dist3 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 9,0))  # 8:00 - 10:00
+                time4, dist4 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 12, 0))  # 10:00 - 14:00
+                time5, dist5 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 15,30))  # 14:00 - 17:00
+                time6, dist6 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 18,30))  # 17:00 - 20:00
+                time7, dist7 = getDistTime(station_name, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, 21, 0))  # 20:00 - 23:00
 
-        my_json.update(stationsJson)
-        print(my_json)
-        file.seek(0)
-        json.dump(my_json, file)
+                stationsJson[f'{station_name}-{station2}'] = {
+                                                            "1:30": (time0, dist0),
+                                                            "4:30": (time1, dist1),
+                                                            "7:00": (time2, dist2),
+                                                            "9:00": (time3, dist3),
+                                                            "12:00": (time4, dist4),
+                                                            "15:30": (time5, dist5),
+                                                            "18:30": (time6, dist6),
+                                                            "21:00": (time7, dist7)
+                                                        }
+                file.seek(0)
+                json.dump(stationsJson, file)
 
 def stationsCalc():
     '''
     :return: initialization of all files and creating them
     '''
-    for station1 in range(1, 71):  # start stations
+    for station1 in range(40, 71):  # start stations
         stationTimes(station1)
 
 def calcDistTime(org, dst, search_time):
@@ -107,95 +117,132 @@ def calcDistTime(org, dst, search_time):
     :param search_time: time search
     :return: meters and min bewteen 2 stations
     '''
-    with open(f"stationsFilesNoTimes/{org}.json", "r") as file:
+    with open(f"stationsFiles/{org}.json", "r") as file:
         data = json.load(file)
-        return data[f'{org}-{dst}']
 
+        time = search_time.strftime("%H:%M")
+        hour, min = time.split(":")
+
+        if hour[0] is '0':
+            hour = hour[1]
+        if min[0] is '0':
+            min = min[1]
+
+        search_time = f'{hour}:{min}'
+
+        if search_time in data[f'{org}-{dst}']:
+            return data[f'{org}-{dst}'][search_time]
+
+        else:
+            hour = int(hour)
+            min = int(min)
+
+            if hour >= 1 and hour < 4: # middle 3:00
+                if hour <= 2 or (hour == 3 and min == 0):
+                    return data[f'{org}-{dst}']['1:30']
+                return data[f'{org}-{dst}']['4:30']
+
+            elif hour >= 4 and hour < 6: # middle 5:15
+                if hour <= 4 or (hour == 5 and min <= 15):
+                    return data[f'{org}-{dst}']['4:30']
+                return data[f'{org}-{dst}']['6:00']
+
+            elif hour >= 6 and hour < 7:
+                if hour == 6 and min <= 30:
+                    return data[f'{org}-{dst}']['6:00']
+                return data[f'{org}-{dst}']['7:00']
+
+            elif hour >= 7 and hour < 8:
+                if hour == 7 and min <= 30:
+                    return data[f'{org}-{dst}']['7:00']
+                return data[f'{org}-{dst}']['8:00']
+
+            elif hour >= 8 and hour < 9:
+                if hour == 8 and min <= 30:
+                    return data[f'{org}-{dst}']['8:00']
+                return data[f'{org}-{dst}']['9:00']
+
+            elif hour >= 9 and hour < 10:
+                if hour == 9 and min <= 30:
+                    return data[f'{org}-{dst}']['9:00']
+                return data[f'{org}-{dst}']['10:00']
+
+            elif hour >= 10 and hour < 11:
+                if hour == 10 and min <= 30:
+                    return data[f'{org}-{dst}']['10:00']
+                return data[f'{org}-{dst}']['11:00']
+
+            elif hour >= 11 and hour < 12:
+                if hour == 11 and min <= 30:
+                    return data[f'{org}-{dst}']['11:00']
+                return data[f'{org}-{dst}']['12:00']
+
+            elif hour >= 12 and hour < 13:
+                if hour == 12 and min <= 30:
+                    return data[f'{org}-{dst}']['12:00']
+                return data[f'{org}-{dst}']['13:30']
+
+            elif hour >= 13 and hour < 15:
+                if hour <= 14 or (hour == 14 and min <= 30):
+                    return data[f'{org}-{dst}']['13:30']
+                return data[f'{org}-{dst}']['15:30']
+
+            elif hour >= 15 and hour < 16:
+                if hour == 15 and min >= 30:
+                    return data[f'{org}-{dst}']['15:30']
+                return data[f'{org}-{dst}']['16:30']
+
+            elif hour >= 16 and hour < 18:
+                if hour <= 17 or (hour == 17 and min <= 30):
+                    return data[f'{org}-{dst}']['16:30']
+                return data[f'{org}-{dst}']['18:30']
+
+            elif hour >= 18 and hour < 21:
+                if hour < 19 or (hour == 19 and min <= 55):
+                    return data[f'{org}-{dst}']['18:30']
+                return data[f'{org}-{dst}']['21:00']
+
+            elif hour >= 21 and hour < 23:
+                if hour < 22 :
+                    return data[f'{org}-{dst}']['21:00']
+                return data[f'{org}-{dst}']['23:00']
+
+            elif hour == 0 and min <= 25:
+                return data[f'{org}-{dst}']['23:00']
+
+            return data[f'{org}-{dst}']['1:30']
+
+
+def addWazeCalls(hours_lst):
+    for station1 in range(1, 71):
+        for station2 in range(1, 71):
+            if(station1 != station2):
+                with open(f"stationsFiles/{station1}.json", "r+") as f:
+                    data = json.load(f)
+                    new_data = {}
+                    for hour in hours_lst:
+                        time = hour.split(":")
+                        new_data.update({f'{hour}':getDistTime(station1, station2,datetime.datetime(initialDate.year, initialDate.month, initialDate.day, int(time[0]),int(time[1])))})
+                    data[f'{station1}-{station2}'].update(new_data) # <--- add hour value.
+                    f.seek(0)  # <--- should reset file position to the beginning.
+                    json.dump(data, f)
+            print("Done with", station1, station2)
 
 if __name__ == '__main__':
 
-    LS_API_KEY = "SFCT9i7qKVVOKzVComo6TZLudYgIqVFdGEVzG7rGgeA" #here api
+    # Run these lines if you want to recalculate hours or add hours to files (!)
+
     # initialize stations from geojson file
-    stations = initStations()
+    # stations = initStations()
+
+    # creating all files with hours waze calls
     # stationsCalc()
 
+    # add hours to json files
+    # addWazeCalls(['6:00', '10:00', '11:00'])
+
     # call func for calc time & route between 2 stations
-    # print(calcTimeDist(1, 2, "yuyu"))
+    print(calcDistTime(46, 1, datetime.datetime(2018, 3, 13, 3, 36)))
 
 
-    service = Directions(access_token=access_token)
-    origin=[34.619748736651466, 31.772700974698733][::-1]
-    destination=[34.666929602032738, 31.811967981669021][::-1]
-    timee=datetime.datetime(2022, 1, 11, 7, 14)
-    response = service.directions([origin, destination],'mapbox/driving-traffic',depart_at=datetime.datetime.timestamp(timee))
-    # print(response.status_code)
-    driving_routes = response.geojson()
-    # print(driving_routes['features'][0]['properties'])
 
-
-    departure_time=datetime.datetime(2022, 1, 2, 9, 0)
-    response = service.directions([origin, destination],'mapbox/driving',depart_at=datetime.datetime.timestamp(timee))
-    # print(response.status_code)
-    driving_routes = response.geojson()
-    print("mapbox")
-    # print(driving_routes['features'][0]['properties'])
-    print(f"duration: {driving_routes['features'][0]['properties']['duration']/60}")
-    print(f"length: {driving_routes['features'][0]['properties']['distance']}")
-
-    import json
-    import os
-
-    from here_location_services import routing_api
-
-    # LS_API_KEY = os.environ.get("LS_API_KEY")  # Get API KEY from environment.
-    RoutingApi = routing_api.RoutingApi(api_key=LS_API_KEY)
-
-    # address = "Invalidenstr 116, 10115 Berlin, Germany"
-    geo = RoutingApi.route(
-        transport_mode= 'car',
-        origin=origin,
-        destination= destination,
-    departure_time=departure_time,
-    return_results = ['summary','typicalDuration'], #  typicalDuration - duration with typical traffic information for the given time of day
-
-    )
-    # print(json.dumps(geo.to_geojson(), indent=2, sort_keys=True))
-    # print(geo)
-    # geo=json.loads(geo.text)
-    # print(geo.status_code)
-    # pprint(geo.json())
-    print("here")
-    print(f"typicalDuration: {geo.json()['routes'][0]['sections'][0]['summary']['typicalDuration']/60}")
-    print(f"length: {geo.json()['routes'][0]['sections'][0]['summary']['length']}")
-
-    # print(json.loads(geo.text))
-
-    # geo = ls.route(
-    #     transport_mode= str,
-
-    #     origin=origin,
-    #     destination= destination,
-    # departure_time=departure_time,
-    # return_results = ['summary','typicalDuration'], #  typicalDuration - duration with typical traffic information for the given time of day
-
-    # )
-    # # print(json.dumps(geo.to_geojson(), indent=2, sort_keys=True))
-    # # print(geo)
-    # # geo=json.loads(geo.text)
-    # # print(geo.status_code)
-    # # pprint(geo.json())
-    # print("here")
-    # print(f"typicalDuration: {geo.json()['routes'][0]['sections'][0]['summary']['typicalDuration']/60}")
-    # print(f"length: {geo.json()['routes'][0]['sections'][0]['summary']['length']}")
-
-    # # print(json.loads(geo.text))
-
-    # # geo = ls.route(
-    # #     transport_mode= str,
-    # #     origin=origin,
-    # #     destination= destination,
-    # #    )
-    # # print(json.dumps(geo.to_geojson(), indent=2, sort_keys=True))
-
-    # print("waze")
-    # print(getDistTime(29, 21, departure_time))
