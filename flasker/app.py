7d559b384e37409ed344d4cd04964105f2fbd7a6
Your Name <ruticohen770@gmail.com>
2022-03-13 23:56:40 +0200

    show parcels movement on map

--- flasker/app.py
+++ flasker/app.py
@@ -11,65 +11,152 @@ app = Flask(__name__)
 @app.route('/')
 def showMapForAll():
 
+    # paths = [
+    #     {
+    #         "driver": 11,
+    #         "path": [
+    #             11,
+    #             14,
+    #             15,
+    #             24,
+    #             22],
+    #         "start": datetime.datetime(2022, 1, 2, 11, 28),
+    #         "times": [
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 11, 28, 0).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 11, 33, 48).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 11, 33, 51).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 11, 41, 17).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 11, 43, 5).timetuple())) * 1000
+    #         ]
+    #     },
+    #     {
+    #         "driver": 23,
+    #         "path": [
+    #             38,
+    #             37,
+    #             36,
+    #             54,
+    #             11
+    #         ],
+    #         "start": datetime.datetime(2022, 1, 2, 9, 26),
+    #         "times": [
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 9, 26, 0).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 9, 27, 1).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 9, 27, 45).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 9, 31, 40).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 9, 32, 38).timetuple())) * 1000
+    #         ]
+    #     },
+    #     {
+    #         "driver": 30,
+    #         "path": [
+    #             7,
+    #             8,
+    #             6,
+    #             4,
+    #             28
+    #         ],
+    #         "start": datetime.datetime(2022, 1, 2, 14, 17),
+    #         "times": [
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 14, 17, 0).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 14, 17, 5).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 14, 18, 12).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 14, 21, 27).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 14, 27, 5).timetuple())) * 1000
+    #         ]
+    #     },
+    #     {
+    #         "driver": 41,
+    #         "path": [
+    #             61,
+    #             14,
+    #             15,
+    #             7,
+    #             17,
+    #             18
+    #         ],
+    #         "start": datetime.datetime(2022, 1, 2, 12, 31),
+    #         "times": [
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 31, 0).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 37, 27).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 37, 30).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 44, 4).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 45, 5).timetuple())) * 1000,
+    #             int(time.mktime(datetime.datetime(
+    #                 2022, 1, 2, 12, 45, 45).timetuple())) * 1000,
+    #         ]
+    #     }
+    # ]
+
+    # results = {
+    #     "2": {
+    #         "path": [
+    #             [54, None],
+    #             [54, 23],
+    #             [11, 23],
+    #             [11, 11],
+    #             [14, 11],
+    #             [14, 41],
+    #             [15, 41],
+    #             [7, 41],
+    #             [7, 30],
+    #             [8, 30],
+    #             [6, 30],
+    #             [4, 30],
+    #             [28, 30],
+    #             [28, None]
+    #         ],
+    #         "startTime": int(time.mktime(datetime.datetime(2022, 1, 2, 9, 20, 0).timetuple())) * 1000
+    #     }
+    # }
+
     paths = [
         {
-            "driver": 11,
-            "path": [
-                11,
-                14,
-                15,
-                24,
-                22],
-            "start": datetime.datetime(2022, 1, 2, 11, 28),
-            "times": [
+            'driver': 41,
+            'path': [61, 14, 15, 7, 17, 18],
+            'start': datetime.datetime(2022, 1, 2, 12, 31),
+            'times': [
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 11, 28, 0).timetuple())) * 1000,
+                    2022, 1, 2, 12, 31).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 11, 33, 48).timetuple())) * 1000,
+                    2022, 1, 2, 12, 37, 27).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 11, 33, 51).timetuple())) * 1000,
+                    2022, 1, 2, 12, 37, 30).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 11, 41, 17).timetuple())) * 1000,
+                    2022, 1, 2, 12, 44, 4).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 11, 43, 5).timetuple())) * 1000
-            ]
+                    2022, 1, 2, 12, 45, 5).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 12, 45, 45).timetuple())) * 1000
+            ],
         },
         {
-            "driver": 23,
-            "path": [
-                38,
-                37,
-                36,
-                54,
-                11
-            ],
-            "start": datetime.datetime(2022, 1, 2, 9, 26),
-            "times": [
+            'driver': 30,
+            'path': [7, 8, 6, 4, 28],
+            'start': datetime.datetime(2022, 1, 2, 14, 17),
+            'times': [
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 9, 26, 0).timetuple())) * 1000,
-                int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 9, 27, 1).timetuple())) * 1000,
-                int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 9, 27, 45).timetuple())) * 1000,
-                int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 9, 31, 40).timetuple())) * 1000,
-                int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 9, 32, 38).timetuple())) * 1000
-            ]
-        },
-        {
-            "driver": 30,
-            "path": [
-                7,
-                8,
-                6,
-                4,
-                28
-            ],
-            "start": datetime.datetime(2022, 1, 2, 14, 17),
-            "times": [
-                int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 14, 17, 0).timetuple())) * 1000,
+                    2022, 1, 2, 14, 17).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
                     2022, 1, 2, 14, 17, 5).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
@@ -81,52 +168,80 @@ def showMapForAll():
             ]
         },
         {
-            "driver": 41,
-            "path": [
-                61,
-                14,
-                15,
-                7,
-                17,
-                18
-            ],
-            "start": datetime.datetime(2022, 1, 2, 12, 31),
-            "times": [
+            'driver': 22,
+            'path': [14, 15, 17, 19, 7, 18, 27],
+            'start': datetime.datetime(2022, 1, 2, 3, 41),
+            'times': [
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 31, 0).timetuple())) * 1000,
+                    2022, 1, 2, 3, 41).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 37, 27).timetuple())) * 1000,
+                    2022, 1, 2, 3, 41, 3).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 37, 30).timetuple())) * 1000,
+                    2022, 1, 2, 3, 46, 6).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 44, 4).timetuple())) * 1000,
+                    2022, 1, 2, 3, 46, 52).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 45, 5).timetuple())) * 1000,
+                    2022, 1, 2, 3, 50, 57).timetuple())) * 1000,
                 int(time.mktime(datetime.datetime(
-                    2022, 1, 2, 12, 45, 45).timetuple())) * 1000,
+                    2022, 1, 2, 3, 52, 37).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 3, 54, 32).timetuple())) * 1000
             ]
+
+        },
+        {
+            'driver': 23,
+            'path': [38, 37, 36, 54, 11],
+            'start': datetime.datetime(2022, 1, 2, 9, 26),
+            'times': [
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 9, 26).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 9, 27, 1).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 9, 27, 45).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 9, 31, 40).timetuple())) * 1000,
+                int(time.mktime(datetime.datetime(
+                    2022, 1, 2, 9, 32, 38).timetuple())) * 1000
+            ]
+
         }
     ]
 
     results = {
-        "2": {
-            "path": [
-                [54, None],
-                [54, 23],
-                [11, 23],
-                [11, 11],
-                [14, 11],
-                [14, 41],
-                [15, 41],
-                [7, 41],
-                [7, 30],
-                [8, 30],
-                [6, 30],
-                [4, 30],
-                [28, 30],
-                [28, None]
+        "15": {
+            'path': [
+                (61, None),
+                (61, 41),
+                (14, 41),
+                (15, 41),
+                (7, 41),
+                (7, 30),
+                (8, 30),
+                (8, None)
             ],
-            "startTime": int(time.mktime(datetime.datetime(2022, 1, 2, 9, 20, 0).timetuple())) * 1000
+            'payActual': 10.348666666666666,
+            'payMax': 12.422,
+            'startTime': int(time.mktime(datetime.datetime(2022, 1, 2, 11, 5).timetuple())) * 1000,
+            'totalDistance': 6422.0,
+            'totalDrivers': 2,
+            'totalDuration': 192.08333333333334
+        },
+        "91": {
+            'path': [
+                (38, None),
+                (38, 23),
+                (37, 23),
+                (36, 23),
+                (36, None)
+            ],
+            'payActual': 3.4065,
+            'payMax': 3.813,
+            'startTime': int(time.mktime(datetime.datetime(2022, 1, 2, 9, 20).timetuple())) * 1000,
+            'totalDistance': 813.0,
+            'totalDrivers': 1,
+            'totalDuration': 530.75,
         }
     }
 
--- flasker/static/map.js
+++ flasker/static/map.js
@@ -6,7 +6,7 @@ L.mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
 
 const MAPBOX_DRIVING_API = "https://api.mapbox.com/directions/v5/mapbox/";
 
-let INITIAL_DATE = new Date(2021, 12, 2, 9, 18, 0, 0); // 7.3.2022
+let INITIAL_DATE = new Date(2021, 12, 2, 11, 4, 0, 0); // 2.1.2022
 const MIN_TO_SEC_RATIO = 1; //TODO: FIX IT TO 4!!! MIN_TO_SEC_RATIO [sec] reality = 60 [sec] simulator
 const SEC_IN_MIN = 60; // each min has 60 sec in reality
 
@@ -26,7 +26,7 @@ function add_minutes(date , minutes) {
 /**
  * @param {*} stations 
  */
-const addStations2map = (stations) => {
+function addStations2map(stations){
 
     for (const station in stations) {
         coord=SP2Coords(station);
@@ -157,12 +157,16 @@ function removeIcon(icon){
  * @param {*} geojson 
  * @param {*} time
  */
-async function navigateFromPointToPoint(driver, geojson, time) {
+async function navigateFromPointToPoint(driver, geojson, time, parcels2Pass) {
     return new Promise(res => {
         let j = 0;
         tick();
         function tick(){
             driver.setLatLng(L.latLng(geojson.coordinates[j][1],geojson.coordinates[j][0]));
+            parcels2Pass.forEach((parcel) => {
+                parcel.parcelIcon.setLatLng(L.latLng(geojson.coordinates[j][1],geojson.coordinates[j][0]));
+            });
+
             if (++j < geojson.coordinates.length) {
                 setTimeout(tick, time * (MIN_TO_SEC_RATIO/SEC_IN_MIN));
             } else {
@@ -173,29 +177,67 @@ async function navigateFromPointToPoint(driver, geojson, time) {
 }
 
 /**
+ * remove un activated driver from activer drivers array
+ * @param {*} activeDrivers 
+ * @param {*} driver 
+ */
+function removeOutdatedDriver(activeDrivers, driver) {
+    const now = INITIAL_DATE.getTime();
+
+    const driverIndex = activeDrivers.findIndex((d) => {
+        d.driver === driver.driver;
+    });
+
+    setTimeout(function(){
+        activeDrivers.splice(driverIndex, 1);
+    }, 1000);
+       
+}
+
+/**
  * @param {{driver: x, path: Array(4), start:--, times: Array(4)}} route 
  * @param {{driver: x, layers: Array(y), driverIcon: e, coordinates: Array[[],[],...]}} newDriver 
  */
-async function animateRoutes(route, newDriver) {
+async function animateRoutes(route, newDriver, activeParcels, activeDrivers) {
 
     for (let index = 0; index < route.path.length - 1; index++) {
         const timeDelta = (route.times[index+1] - route.times[index]);
         const coordDelta = newDriver.coordinates[index];
         const travelTimeRatio = Math.floor(timeDelta/coordDelta.coordinates.length);
+
+        let parcels2Pass = activeParcels.filter((parcel) => {
+            const driverNum = route.driver;
+            const station1 = route.path[index];
+            const station2 =  route.path[index+1];
+
+            for (let station = 0; station < parcel.path.length - 1; station++) {
+                const firstMatch =  parcel.path[station]; // <station, driver>
+                const secondMatch = parcel.path[station+1]; // <station, driver>
+
+                // if there is a match between driver to parcel
+                if(station1 === firstMatch[0] && station2 === secondMatch[0] && driverNum === firstMatch[1] && driverNum === secondMatch[1]){
+                    return parcel;
+                }
+            }
+        });
         
-        await navigateFromPointToPoint(newDriver.driverIcon, coordDelta, travelTimeRatio);
+        await navigateFromPointToPoint(newDriver.driverIcon, coordDelta, travelTimeRatio, parcels2Pass);
+        // removeIcon(newDriver.layers[index]);
     }
 
-    console.log("harrrayyyyy")
+    console.log("harrrayyyyy");
 
     // after all route animation - delete route & thier drivers icons
     removeCompleteRoutes(newDriver.layers);
     removeIcon(newDriver.driverIcon);
+    removeOutdatedDriver(activeDrivers, newDriver);
 }
 
-async function refreshRoutes(routes, activeDrivers) {
+async function refreshRoutes(routes, activeDrivers, activeParcels) {
     const newActive = newActiveRoutes(routes, activeDrivers);
-    console.log(newActive);
+
+    console.log("***active drivers:", activeDrivers);
+    console.log("**new active:", newActive);
 
     newActive.forEach(async (route) => {
         const points = servicePointsToCoordinates(route.path);
@@ -213,7 +255,7 @@ async function refreshRoutes(routes, activeDrivers) {
         };
         activeDrivers.push(newDriver);
 
-        animateRoutes(route, newDriver);
+        animateRoutes(route, newDriver, activeParcels, activeDrivers);
     });
 }
 
@@ -225,8 +267,8 @@ function newActiveParcels(parcels, newActiveParcels) {
     const now = INITIAL_DATE.getTime();
     return Object.keys(parcels).filter((parcelNum) => {        
         return parcels[parcelNum]["startTime"] <= now &&
-        // !newActiveParcels.includes({parcelNum});
         !(newActiveParcels.findIndex(p => p.num === parcelNum) > -1);
+        // parcels[parcelNum]["path"] > 1; // TODO: fix that - Mussi 
     });
 }
 
@@ -234,18 +276,91 @@ function newActiveParcels(parcels, newActiveParcels) {
  * show parcels on map in specific time
  * @param {*} results
  */
-function refreshParcels(results, activeParcels) {
+function refreshParcels(results, activeParcels, routes) {
     const newActive =  newActiveParcels(results, activeParcels);
+    
     newActive.forEach((parcelName) => {
-        const layer = displayParcel(SP2Coords(results[parcelName].path[0][0]), parcelName);
-        
+        const parcelIcon = displayParcel(SP2Coords(results[parcelName].path[0][0]), parcelName);
+
+        // extract parcel transfer end time 
+        const lastParcelDriver = results[parcelName].path[results[parcelName].path.length - 2][1];
+        const lastParcelDriverStation = results[parcelName].path[results[parcelName].path.length - 2][0];
+        const parcelDriver = routes.filter(d => d.driver === lastParcelDriver)[0]; //{routes[driver]}
+        const driverStationTimeIndex = parcelDriver.path.findIndex(station => station === lastParcelDriverStation);
+        const parcelFinishTime = parcelDriver.times[driverStationTimeIndex] + 2000; // remove parcel 2 sec later ... 
+
         const newParcel = {
             num: parcelName,
-            layer: layer,
+            parcelIcon: parcelIcon,
+            path: results[parcelName].path,
+            finishPathTime: parcelFinishTime,
         };
 
         activeParcels.push(newParcel);
     });
+}
+
+/**
+ * update time on map
+ */
+function clockTime() {
+
+    const hour = (INITIAL_DATE.getHours() < 10) ? ("0" + INITIAL_DATE.getHours()) : INITIAL_DATE.getHours();
+    const min = (INITIAL_DATE.getMinutes() < 10) ? ("0" + INITIAL_DATE.getMinutes()) : INITIAL_DATE.getMinutes();
+    const day = INITIAL_DATE.getUTCDate();
+    const month = INITIAL_DATE.getUTCMonth() + 1;
+    const year = INITIAL_DATE.getFullYear();
+
+    document.getElementById("clock").innerHTML =`${hour}:${min}  ${day}/${month}/${year}`;
+    setTimeout(clockTime, 1000);
+}
+
+/**
+ * this function remove all parcels when they finish path
+ * @param {*} activeParcels 
+ */
+function removeParcelWhenEnds(activeParcels, arrivedSuccessfullyParcels) {
+    const now = INITIAL_DATE.getTime();
+    const removedParcels = activeParcels.filter(p => p.finishPathTime < now);
+    
+    removedParcels.forEach(p => {
+        removeIcon(p.parcelIcon);
+        const parcelIndex = activeParcels.findIndex(parcel => parcel.num === p.num);
+
+        // add a parcel only if doesnt exist 
+        if(arrivedSuccessfullyParcels.findIndex(parcel => p.num === parcel.num) === -1){
+            arrivedSuccessfullyParcels.push(p);
+        }
+        
+        activeParcels.splice(parcelIndex, 1);
+    });
+}
+
+
+/**
+ * update the text-box on map to show activeDrivers
+ * @param {*} activeDrivers 
+ */
+function updateHTML(activeDrivers, activeParcels, arrivedSuccessfullyParcels) {
+    // active drivers
+    const driversNow = activeDrivers.map((d) => {
+        return d.driver;
+    });
+
+    // active parcels
+    const parcelsNow = activeParcels.map((p) => {
+        return p.num;
+    });
+
+    // arrived parcels
+    const parcelOld = arrivedSuccessfullyParcels.map((p) => {
+        return p.num;
+    });
+
+    
+    // if(activeNow){
+    //     document.getElementById("active-drivers").innerHTML = "<h4>active drivers</h4>" + "<br />" + activeNow;
+    // }
 }
 
 const main = async () => {
@@ -255,16 +370,24 @@ const main = async () => {
     
     let activeDrivers = [];
     let activeParcels = [];
+    let arrivedSuccessfullyParcels = [];
 
-    refreshRoutes(routes, activeDrivers);
-    refreshParcels(results, activeParcels);
+    refreshRoutes(routes, activeDrivers, activeParcels);
+    refreshParcels(results, activeParcels, routes);
 
     setInterval(() => {
-        refreshRoutes(routes, activeDrivers);
-        refreshParcels(results, activeParcels);
+
         INITIAL_DATE = add_minutes(INITIAL_DATE, 1);
+
+        refreshRoutes(routes, activeDrivers, activeParcels);
+        refreshParcels(results, activeParcels, routes);
+        removeParcelWhenEnds(activeParcels, arrivedSuccessfullyParcels);
         
-        console.log(INITIAL_DATE);
+        updateHTML(activeDrivers, activeParcels, arrivedSuccessfullyParcels);
+        
+        // console.log(activeParcels, '----');
+        // console.log(activeDrivers, '^^^^^^');
+        // console.log(arrivedSuccessfullyParcels, '++++');
 
     }, 1000 * MIN_TO_SEC_RATIO);
     
