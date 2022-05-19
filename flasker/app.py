c72b7f5093459232efd73f45900ff667706a2f99
Your Name <ruticohen770@gmail.com>
2022-05-19 21:51:49 +0300

    adding end simulation page & tiny more fixes in the app

--- flasker/app.py
+++ flasker/app.py
@@ -10,27 +10,33 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 DRIVERS_DETAILS_DEFAULT = {"drivers-amount":300, "parcels-amount":600, "speed":1}
 
+
 @app.route('/')
-
-
 def homepage():
     session["drivers-details"] = DRIVERS_DETAILS_DEFAULT
     return render_template('homepage.html')
 
 @app.route('/introduction')
-def intro():
+def introduction():
     return render_template('introduction.html')
 
 @app.route('/simulation-details')
-def start():
+def menu1():
     if session.get("drivers-details"):
         results = session.get('drivers-details')
         return render_template('menu1.html', driversNum=results["drivers-amount"], parcelsNum=results["parcels-amount"], speed=results["speed"]) 
     return render_template('menu1.html', driversNum=300, parcelsNum=600, speed=1) 
 
 
+@app.route('/drivers-details', methods=['POST'])
+def menu2():
+    result = request.form.to_dict()
+    print(result)
+    session["drivers-details"] = result
+    return render_template('menu2.html',driver_num=result["drivers-amount"],parcels_num=result["parcels-amount"],speed=result["speed"])
+
 @app.route('/map', methods=['POST', 'GET'])
-def result():
+def map():
     if request.method == 'POST':
         return ('/map') 
 
@@ -40,10 +46,6 @@ def result():
     drivers, results, parcels = getFiles(args['driversNum'], args['parcelsNum'], args['userChoice'])
     return render_template('map.html', stations=sp.stations, paths=drivers, results=results, speedRatio=convertSpeedToRatio(args['speed']))
 
-
-@app.route('/drivers-details', methods=['POST'])
+@app.route('/end-simulation')
 def end():
-    result = request.form.to_dict()
-    print(result)
-    session["drivers-details"] = result
-    return render_template('menu2.html',driver_num=result["drivers-amount"],parcels_num=result["parcels-amount"],speed=result["speed"])
+    return render_template('end.html')
